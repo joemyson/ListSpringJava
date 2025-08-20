afe43d91fcf5ef7d1ae18698ab7906fbfde5e87f
joemyson <joemyson1402@gmail.com>
2025-08-20 15:40:11 -0300

    iniciado projetoJavaSpring

---
+++ src/test/java/com/JSC/dsList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JSC.dsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
